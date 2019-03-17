84bc4ced6b85581248a137544a7b58859b776251
Brent Mifsud <brentmifsud@icloud.com>
2019-03-17 14:22:22 -0400

    wire up outlets

---
+++ Meme Me 1.0/Delegates/TextFieldDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  TextfieldDelegate.swift
+//  Meme Me 1.0
+//
+//  Created by Brent Mifsud on 2019-03-17.
+//  Copyright © 2019 Brent Mifsud. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        if textField.text == "TOP" || textField.text == "BOTTOM" {
+            textField.text = ""
+        }
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        return true
+        
+    }
+    
+}
--- Meme Me 1.0/ViewController.swift
+++ Meme Me 1.0/ViewController.swift
@@ -10,9 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK:- Outlets
+    @IBOutlet weak var navigationBar: UINavigationItem!
+    @IBOutlet weak var actionButton: UIBarButtonItem!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var toolbar: UIToolbar!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var galleryButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let textFieldDelegate = TextFieldDelegate()
+    
+    let textAttributes: [NSAttributedString.Key:Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "Impact", size: 40)!,
+        NSAttributedString.Key.strokeWidth: -5
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        initTextField(textField: topTextField, text: "TOP")
+        initTextField(textField: bottomTextField, text: "BOTTOM")
+    }
+    
+    func initTextField(textField: UITextField, text: String){
+        textField.delegate = textFieldDelegate
+        textField.defaultTextAttributes = textAttributes
+        textField.text = text
+        textField.textAlignment = .center
     }
 
 
