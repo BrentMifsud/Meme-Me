65de52b86d48bcc26f61dc8c79e254cf9212de84
Brent Mifsud <brentmifsud@icloud.com>
2019-03-17 14:47:49 -0400

    delete blank lines

--- Meme Me 1.0/Delegates/TextFieldDelegate.swift
+++ Meme Me 1.0/Delegates/TextFieldDelegate.swift
@@ -11,18 +11,14 @@ import UIKit
 class TextFieldDelegate: NSObject, UITextFieldDelegate {
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        
         if textField.text == "TOP" || textField.text == "BOTTOM" {
             textField.text = ""
         }
-        
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        
         textField.resignFirstResponder()
         return true
-        
     }
     
 }
