362521125621a5fe88eee3626515fa9a71893fce
Brent Mifsud <brentmifsud@icloud.com>
2019-04-01 22:18:51 -0400

    add meme table view controller

---
+++ Meme Me/Controller/MemesTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MemeTableViewController.swift
+//  Meme Me
+//
+//  Created by Brent Mifsud on 2019-04-01.
+//  Copyright © 2019 Brent Mifsud. All rights reserved.
+//
+
+import UIKit
+
+class MemeTableViewController: UITableViewController {
+
+	var memes: [Meme]! {
+		let appDelegate = UIApplication.shared.delegate as! AppDelegate
+		return appDelegate.memes
+	}
+
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+		let cell = tableView.dequeueReusableCell(withIdentifier: "memeCell")
+		cell?.imageView!.image = memes[indexPath.row].memedImage
+		cell?.textLabel!.text = memes[indexPath.row].topText + " " + memes[indexPath.row].bottomText
+
+		return cell!
+	}
+
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+		let appDelegate = UIApplication.shared.delegate as! AppDelegate
+
+		return appDelegate.memes.count
+	}
+}
