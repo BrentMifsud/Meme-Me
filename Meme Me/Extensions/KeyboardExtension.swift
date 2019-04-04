df1a938dd36e1114228258459101ac88fd54a5dd
Brent Mifsud <brentmifsud@icloud.com>
2019-04-03 23:08:20 -0400

    add tap gesture recognizer to hide keyboard when tapping outside of a textfield

--- Meme Me/Extensions/KeyboardExtension.swift
+++ Meme Me/Extensions/KeyboardExtension.swift
@@ -11,11 +11,15 @@ extension MemeEditorView {
 	func subscribeToKeyboardNotifications() {
 		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
 		NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+
+		self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissKeyboardOnTap)))
 	}
 
 	func unsubscribeFromKeyboardNotifications() {
 		NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
 		NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+
+		self.view.removeGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissKeyboardOnTap)))
 	}
 
 	@objc func keyboardWillShow(_ notification: Notification) {
@@ -30,6 +34,10 @@ extension MemeEditorView {
 		if bottomTextField.isEditing {
 			view.frame.origin.y = 0
 		}
+	}
+
+	@objc func dismissKeyboardOnTap(){
+		self.view.endEditing(true)
 	}
 
 	private func getKeyboardHeight(_ notification: Notification) -> CGFloat {
